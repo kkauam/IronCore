78a39e2301ec070a8c9263536c5a1b521af235d7
Enzo <enzohenriconunesferreira@gmail.com>
2026-04-01 18:56:43 -0300

    Compartilhar arquivos do projeto

---
+++ src/main/java/health/IronCore/IronCoreApplication.java
@@ -0,0 +1,13 @@
+package health.IronCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IronCoreApplication.class, args);
+	}
+
+}
